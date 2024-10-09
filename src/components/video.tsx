abc104fe5d9832ea46e053e34385ac1946939dc1
saungnaga <saungnagaa@gmail.com>
2024-10-09 13:32:47 +0700

    fixing bugs

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,5 +1,4 @@
-import Movie from "../assets/Movie.mp4";
-import movie from "../assets/movie.jpg";
+
 
 function Video() {
   return (
@@ -7,7 +6,6 @@ function Video() {
       <iframe
         src="https://www.youtube.com/embed/THOEh1Qvcrk"
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
